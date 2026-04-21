24c7d45a75ae7d0e55fd4cffed7e4561f8f702f9
wewe-web <eiyanguray@gmail.com>
2026-04-21 19:26:05 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'vigilo-v1';
+const ASSETS = [
+  'index.html',
+  'logo.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
